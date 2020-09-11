d25fe9967ee02006258b6968050544e38dc3bd23
Zachary E. Graber <zegraber@iu.edu>
2020-09-11 18:25:47 -0400

    finished Lab3

--- L3/src/Counter.java
+++ L3/src/Counter.java
@@ -22,7 +22,19 @@ public class Counter {
     }
 
     void inc () {
-        // implement inc here
+        boolean done = false;
+        int i = 0; // Start with the first index
+        // This will flip bits until it reaches a 0 (the 0 is flipped to a 1)
+        while (!done && i < this.array.length) {
+            if (this.array[i] == 0) {
+                this.array[i] = 1;
+                done = true;
+            }
+            else {
+                this.array[i] = 0;
+            }
+            i += 1;
+        }
     }
 
     public String toString () {
--- L3/tests/CounterTest.java
+++ L3/tests/CounterTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,4 +40,50 @@ class CounterTest {
     }
 
     // create amortization tests here
+    @Test
+    void incrementTimeTest () {
+
+        // This test will show that the time complexity of incrementing remains amortized constant as the Counter's decimal value increases.
+        // This says that there is essentially no positive correlation between the size of the number and time taken to increment.
+        assertTrue(leastSquaresSlope(timeCounterInIncrements(24, 1000)) < 0.001);
+        assertTrue(leastSquaresSlope(timeCounterInIncrements(24, 100)) < 0.001);
+        assertTrue(leastSquaresSlope(timeCounterInIncrements(24, 10000)) < 0.001);
+    }
+
+    private static long[] timeCounterInIncrements (int size, int dataPoints) {
+        final long MAX_DEC_VAL = (long) Math.pow(2, size) - 1;
+
+        Counter c = new Counter(size);
+        long[] times = new long[dataPoints];
+        Instant start, stop;
+
+        for (int i = 0; i < dataPoints; i++) {
+            start = Instant.now();
+            for (int j = 0; j < MAX_DEC_VAL / dataPoints; j++) {
+                c.inc();
+            }
+            stop = Instant.now();
+            times[i] = Duration.between(start, stop).toMillis();
+        }
+        return times;
+    }
+
+    private static double leastSquaresSlope(long[] vals) {
+        long sumOfXVals = 0, sumOfXSquaredVals = 0, sumOfYVals = 0, sumOfXYProducts = 0;
+        int n = vals.length;
+        long x; // The size of the queue
+        long y; // The time taken for these 100,000 operations
+        for (int i = 0; i < n; i++) {
+            x = 100000 * i;
+            y = vals[i];
+
+            sumOfXVals += x;
+            sumOfYVals += y;
+            sumOfXSquaredVals += Math.pow(x, 2);
+            sumOfXYProducts += x * y;
+        }
+
+        // Formula for best squares method found at https://mathbitsnotebook.com/Algebra1/StatisticsReg/ST2lCalculatorBest.html
+        return ((double) (n * sumOfXYProducts) - (sumOfXVals * sumOfYVals)) / ((n * sumOfXSquaredVals) - (Math.pow(sumOfXVals, 2)));
+    }
 }
