360c178a9d8fa92756ce90772958eacd5f1031ed
Kailen Dobias <kdobias@iu.edu>
2020-10-16 08:40:37 -0400

    L8 starter code

---
+++ L8/src/Traversal.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+class Traversal<E> {
+
+    static <E> List<E> inOrder(BinTree<E> tree){
+        return new Empty<>();
+    }
+
+    static <E> List<E> preOrder(BinTree<E> tree){
+        return new Empty<>();
+    }
+
+    static <E> List<E> postOrder(BinTree<E> tree) {
+        return new Empty<>();
+    }
+
+}
